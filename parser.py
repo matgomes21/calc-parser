e60ffa446a1ac05396543889127046a146600d3c
Mateus Gomes <mateusgomes.nascimento21@gmail.com>
2021-05-21 23:15:52 -0300

    create grammar

--- parser.py
+++ parser.py
@@ -2,21 +2,43 @@ import re
 import math
 from lark import Lark, InlineTransformer, Token
 
-
 # Implemente a gramática aqui! Você pode testar manualmente seu código executando
 # o arquivo calc.py e testá-lo utilizando o pytest.
 grammar = Lark(
     r"""
-start : /\d+/
-""",
-    parser="lalr",
+    ?start  : assign* comp?
+    ?assign: NAME "=" comp
+    ?comp  : expr "<" expr  -> lt
+        | expr "<=" expr -> le
+        | expr ">" expr  -> gt
+        | expr ">=" expr -> ge
+        | expr "!=" expr -> ne
+        | expr "==" expr -> eq
+        | expr
+    ?term  : term "*" pow   -> mul
+        | term "/" pow   -> div
+        | pow
+    ?expr  : expr "-" term  -> sub
+        | expr "+" term  -> add
+        | term
+    ?pow   : atom "^" pow   -> exp
+        | atom
+    ?atom  : NUMBER                        -> number
+        | NAME "(" expr ")"             -> func
+        | NAME "(" expr ("," expr)* ")" -> func
+        | NAME                          -> var
+        | "(" expr ")"
+    NAME   : /[-+]?\w+/
+    NUMBER : /-?(?:0|\d\d*)(?:\.\d+)?(?:[eE][+-]?\d+)?/
+    %ignore /\s+/
+    %ignore /\#.*/
+    """
 )
-
-
 class CalcTransformer(InlineTransformer):
-    from operator import add, sub, mul, truediv as div  # ... e mais! 
+    from operator import add, sub, mul, truediv as div, pow as exp, gt, ge, lt, le, ne, eq
 
     def __init__(self):
         super().__init__()
         self.variables = {k: v for k, v in vars(math).items() if not k.startswith("_")}
         self.variables.update(max=max, min=min, abs=abs)
+        
